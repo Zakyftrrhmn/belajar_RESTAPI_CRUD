f8cf32bd13311490409a4482c8b02253ebfec67a
Zakyftrrhmn <zakifaturrahman0@gmail.com>
2025-05-08 15:13:33 +0700

    update Route API

--- routes/api.php
+++ routes/api.php
@@ -8,26 +8,30 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-// http://127.0.0.1:8000/api/mahasiswa
-// route::get('/mahasiswa', function () {
-//     return 'ok';
-// });
+// // http://127.0.0.1:8000/api/mahasiswa
+// // route::get('/mahasiswa', function () {
+// //     return 'ok';
+// // });
 
-// http://127.0.0.1:8000/api/mahasiswa
-route::get('/mahasiswa', [MahasiswaController::class, 'index']);
+// // http://127.0.0.1:8000/api/mahasiswa
+// route::get('/mahasiswa', [MahasiswaController::class, 'index']);
 
-// tampilkan data berdasarkan id
-// http://127.0.0.1:8000/api/mahasiswa/1
-route::get('/mahasiswa/{mahasiswa}', [MahasiswaController::class, 'show']);
+// // tampilkan data berdasarkan id
+// // http://127.0.0.1:8000/api/mahasiswa/1
+// route::get('/mahasiswa/{mahasiswa}', [MahasiswaController::class, 'show']);
 
-// tambah data
-// http://127.0.0.1:8000/api/mahasiswa
-route::post('/mahasiswa', [MahasiswaController::class, 'store']);
+// // tambah data
+// // http://127.0.0.1:8000/api/mahasiswa
+// route::post('/mahasiswa', [MahasiswaController::class, 'store']);
 
-// ubah data
-// http://127.0.0.1:8000/api/mahasiswa/1
-route::put('/mahasiswa/{mahasiswa}', [MahasiswaController::class, 'update']);
+// // ubah data
+// // http://127.0.0.1:8000/api/mahasiswa/1
+// route::put('/mahasiswa/{mahasiswa}', [MahasiswaController::class, 'update']);
 
-// hapus data
-// http://127.0.0.1:8000/api/mahasiswa/1
-route::delete('/mahasiswa/{mahasiswa}', [MahasiswaController::class, 'destroy']);
+// // hapus data
+// // http://127.0.0.1:8000/api/mahasiswa/1
+// route::delete('/mahasiswa/{mahasiswa}', [MahasiswaController::class, 'destroy']);
+
+
+// Persingkat semua route
+Route::apiResource('mahasiswa', MahasiswaController::class);
